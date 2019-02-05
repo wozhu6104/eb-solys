0a5a3e2976d2b5555d43faf162f9f51964eef029
Raphael Geissler <raphael.geissler@elektrobit.com>
2019-02-05 15:18:13 +0100

    Adding hashcode and equals.
    Otherwise selection in channels legend is not working...

--- src/com.elektrobit.ebrace.viewer.common/src/com/elektrobit/ebrace/viewer/common/provider/ChannelValueProvider.java
+++ src/com.elektrobit.ebrace.viewer.common/src/com/elektrobit/ebrace/viewer/common/provider/ChannelValueProvider.java
@@ -47,4 +47,43 @@ public class ChannelValueProvider
     {
         return decodedNodes;
     }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((channel == null) ? 0 : channel.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        ChannelValueProvider other = (ChannelValueProvider)obj;
+        if (channel == null)
+        {
+            if (other.channel != null)
+            {
+                return false;
+            }
+        }
+        else if (!channel.equals( other.channel ))
+        {
+            return false;
+        }
+        return true;
+    }
 }
