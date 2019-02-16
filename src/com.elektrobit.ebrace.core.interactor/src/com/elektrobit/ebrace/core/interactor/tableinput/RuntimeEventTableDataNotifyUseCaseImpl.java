1b596e083082c3a6d8b3fbe46247e66773732e30
Raphael Geissler <raphael.geissler@elektrobit.com>
2019-02-16 16:56:44 +0100

    Updating tables not to often.

--- src/com.elektrobit.ebrace.core.interactor/src/com/elektrobit/ebrace/core/interactor/tableinput/RuntimeEventTableDataNotifyUseCaseImpl.java
+++ src/com.elektrobit.ebrace.core.interactor/src/com/elektrobit/ebrace/core/interactor/tableinput/RuntimeEventTableDataNotifyUseCaseImpl.java
@@ -58,7 +58,7 @@ public class RuntimeEventTableDataNotifyUseCaseImpl
             FilterScriptCallerListener,
             ChannelsContentChangedListener
 {
-    private static final int UPDATE_TIMER_PERIOD_MS = 1000;
+    private static final int UPDATE_TIMER_PERIOD_MS = 2000;
 
     private RuntimeEventTableDataNotifyCallback callback;
 
